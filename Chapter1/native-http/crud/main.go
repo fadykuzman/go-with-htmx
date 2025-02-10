20c55139136fb118743a35ea978d2abeceb52bf7
Fady Kuzman <fady.kuzman@pm.me>
2025-02-10 22:46:40 +0100

    add style and sort list

--- Chapter1/native-http/crud/main.go
+++ Chapter1/native-http/crud/main.go
@@ -5,6 +5,8 @@ import (
 	"html/template"
 	"log"
 	"net/http"
+	"slices"
+	"strings"
 
 	"github.com/google/uuid"
 	"github.com/gorilla/mux"
@@ -21,24 +23,37 @@ type Dogs map[string]Dog
 var dogMap = make(Dogs)
 
 func GetDogs(w http.ResponseWriter, r *http.Request) {
-
+	dogsSlice := make([]Dog, 0, len(dogMap))
+	for _, v := range dogMap {
+		dogsSlice = append(dogsSlice, v)
+	}
+	slices.SortStableFunc(dogsSlice, func(a, b Dog) int {
+		return strings.Compare(strings.ToLower(a.Name), strings.ToLower(b.Name))
+	})
 	tmpl := template.Must(template.ParseFiles("public/dogs.html"))
-	tmpl.Execute(w, dogMap)
+	tmpl.Execute(w, dogsSlice)
 }
 
-func AddDog(name, breed string, dogs Dogs) {
+func AddDog(name, breed string, dogs Dogs) Dog {
 	dog := Dog{
 		Id:    uuid.NewString(),
 		Name:  name,
 		Breed: breed,
 	}
 	dogs[dog.Id] = dog
+	return dog
 }
 
 func createDog(w http.ResponseWriter, r *http.Request) {
 	name := r.FormValue("name")
 	breed := r.FormValue("breed")
-	AddDog(name, breed, dogMap)
+	dog := AddDog(name, breed, dogMap)
+	slice := []Dog{
+		dog,
+	}
+	tmpl := template.Must(template.ParseFiles("public/dogs.html"))
+	tmpl.Execute(w, slice)
+
 }
 
 func deleteDog(w http.ResponseWriter, r *http.Request) {
@@ -49,14 +64,17 @@ func deleteDog(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-	r := mux.NewRouter().StrictSlash(true)
 	AddDog("Rocky", "Whippet", dogMap)
 	AddDog("Uranu", "Chesterfield", dogMap)
-	fs := http.FileServer(http.Dir("public"))
+
+	r := mux.NewRouter().StrictSlash(true)
 	r.HandleFunc("/dogs/", GetDogs).Methods("GET")
 	r.HandleFunc("/dog", createDog).Methods("POST")
 	r.HandleFunc("/dog/{id}", deleteDog).Methods("DELETE")
 
+	fs := http.FileServer(http.Dir("public"))
+
 	r.PathPrefix("/").Handler(http.StripPrefix("", fs))
+
 	log.Fatal(http.ListenAndServe(":8081", r))
 }
