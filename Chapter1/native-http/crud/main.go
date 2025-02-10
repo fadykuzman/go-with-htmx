9ea1e8d1e40df5dba4871835b0a097192ceb4dff
Fady Kuzman <fady.kuzman@pm.me>
2025-02-10 18:58:00 +0100

    Implement a form and table Add and view

--- Chapter1/native-http/crud/main.go
+++ Chapter1/native-http/crud/main.go
@@ -6,6 +6,7 @@ import (
 	"net/http"
 
 	"github.com/google/uuid"
+	"github.com/gorilla/mux"
 )
 
 type Dog struct {
@@ -18,7 +19,7 @@ type Dogs map[string]Dog
 
 var dogMap = Dogs{}
 
-func dogs(w http.ResponseWriter, r *http.Request) {
+func GetDogs(w http.ResponseWriter, r *http.Request) {
 
 	tmpl := template.Must(template.ParseFiles("public/dogs.html"))
 	tmpl.Execute(w, dogMap)
@@ -33,12 +34,20 @@ func AddDog(name, breed string, dogs Dogs) {
 	dogs[dog.Id] = dog
 }
 
+func createDog(w http.ResponseWriter, r *http.Request) {
+	name := r.FormValue("name")
+	breed := r.FormValue("breed")
+	AddDog(name, breed, dogMap)
+}
+
 func main() {
+	r := mux.NewRouter().StrictSlash(true)
 	AddDog("Rocky", "Whippet", dogMap)
 	AddDog("Uranu", "Chesterfield", dogMap)
-
 	fs := http.FileServer(http.Dir("public"))
-	http.Handle("/", http.StripPrefix("", fs))
-	http.HandleFunc("/dogs", dogs)
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	r.HandleFunc("/dogs/", GetDogs).Methods("GET")
+	r.HandleFunc("/dog", createDog).Methods("POST")
+
+	r.PathPrefix("/").Handler(http.StripPrefix("", fs))
+	log.Fatal(http.ListenAndServe(":8081", r))
 }
